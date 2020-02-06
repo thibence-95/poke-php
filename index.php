7cf295ee04c4ae4115243374a259d98f1c3bd785
Thibence <salencethibaut95@gmail.com>
2020-02-06 12:16:36 +0100

    had some help from learned functions and figuring out assoc arrays

--- index.php
+++ index.php
@@ -2,156 +2,180 @@
 
 $input = $_GET['input'];
 
-$getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/'.$input);
+function getData($inputpoke)
+{
+    $getData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $inputpoke);
+    $data = json_decode($getData, true);
 
-$data = json_decode($getData,true);
-$pokeName = $data['species']['name'];
-$pokeID = $data['id'];
-$pokesprite = $data['sprites']['front_default'];
+    $pokeName = $data['species']['name'];
+    $pokeID = $data['id'];
+    $pokeSprite = $data['sprites']['front_default'];
 
-//$randomMoves = $data['abilities'][mt_rand(0, count($data['abilities']) - 4)];
-//var_dump($randomMoves);
+    return [
+        'name' => $pokeName,
+        'id' => $pokeID,
+        'sprite' => $pokeSprite,
+    ];
+}
 
+
+if ($_GET['input'] = null) {
+
+    //echo $data['results']['0'];
+    getData('name');
+} else {
+
+    $pokemon = getData($input);
+
+}
+?>
+
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PokéDex</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+		      crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/style.css">
+	</head>
+
+	<body>
+
+	<div id="jumbotron">
+		<header class="text-center">
+			<h1 style="font-size: 50px; font-weight: 600;">PokéDex</h1>
+		</header>
+	</div>
+
+	<div class="container" id="promptContainer">
+		<div class="row">
+			<h3 id="find">Find a Pokémon</h3>
+			<span id="error"></span>
+		</div>
+		<form>
+			<div class="row form-group form-inline" id="inputDiv">
+				<input type="text" name="input" id="input" class="form-control"
+				       placeholder="Don't use capital letters, please">
+				<button id="submit" class="btn btn-primary">Search</button>
+			</div>
+		</form>
+	</div>
+
+	<div id="pokedex">
+
+		<!--//// L E F T  P A R T ////-->
+		<div id="left">
+			<div id="logo"></div>
+			<div id="bg_curve1_left"></div>
+			<div id="bg_curve2_left"></div>
+			<div id="curve1_left">
+				<div id="buttonGlass">
+					<div id="reflect"></div>
+				</div>
+				<div id="miniButtonGlass1"></div>
+				<div id="miniButtonGlass2"></div>
+				<div id="miniButtonGlass3"></div>
+			</div>
+			<div id="curve2_left">
+				<div id="junction">
+					<div id="junction1"></div>
+					<div id="junction2"></div>
+				</div>
+			</div>
+			<div id="screen">
+				<div id="topPicture">
+					<div id="buttontopPicture1"></div>
+					<div id="buttontopPicture2"></div>
+				</div>
+				<div id="picture">
+					<img id="sprite" src="<?php echo $pokesprite; ?>"/>
+
+				</div>
+				<div id="buttonbottomPicture"></div>
+				<div id="speakers">
+					<div class="sp"></div>
+					<div class="sp"></div>
+					<div class="sp"></div>
+					<div class="sp"></div>
+				</div>
+			</div>
+			<div id="bigbluebutton"></div>
+			<div id="barbutton1"></div>
+			<div id="barbutton2"></div>
+			<div id="cross">
+				<div id="leftcross">
+					<div id="leftT"></div>
+				</div>
+				<div id="topcross">
+					<div id="upT"></div>
+				</div>
+				<div id="rightcross">
+					<div id="rightT"></div>
+				</div>
+				<div id="midcross">
+					<div id="midCircle"></div>
+				</div>
+				<div id="botcross">
+					<div id="downT"></div>
+				</div>
+			</div>
+		</div>
+
+		<!--//// R I G H T  P A R T ////-->
+		<div id="right">
+			<div id="stats" class="row">
+				<article>
+					<div class="col">
+						<div id="submit-output">
+							<p><?php echo $pokeName; ?></p>
+							<p id="pokéId"><?php echo $pokeID; ?></p>
+						</div>
+
+						<div id="moves">
+							<!--<ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>-->
+						</div>
+					</div>
+					<div class="col">
+						<div class="evolution" id="evolution">
+						</div>
+					</div>
+				</article>
+			</div>
+			<div id="blueButtons1">
+
+			</div>
+
+			<div id="yellowBox1"></div>
+			<div id="yellowBox2"></div>
+			<div id="bg_curve1_right"></div>
+			<div id="bg_curve2_right"></div>
+			<div id="curve1_right"></div>
+			<div id="curve2_right"></div>
+		</div>
+	</div>
+
+
+	<script rel="script" src="script.js" lang="js"></script>
+	</body>
+	</html>
+
+
+<?php
+/*$pokeMoves = array_rand($data['abilities'][,int $num = 4]):mixed;
+var_dump($pokeMoves);*/
 
 /*$ab = $decode['pokemon']['ditto']['abilities']['0']['ability'];
 
 foreach ($ab as $values) {
-    echo $values;
+echo $values;
 }*/
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>PokéDex</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/style.css">
-</head>
-
-<body>
-
-<div id="jumbotron">
-    <header class="text-center">
-        <h1 style="font-size: 50px; font-weight: 600;">PokéDex</h1>
-    </header>
-</div>
-
-<div class="container" id="promptContainer">
-    <div class="row">
-        <h3 id="find">Find a Pokémon</h3>
-        <span id="error"></span>
-    </div>
-	<form>
-		<div class="row form-group form-inline" id="inputDiv">
-			<input type="text" name="input" id="input" class="form-control" placeholder="Don't use capital letters, please">
-			<button id="submit" class="btn btn-primary">Search</button>
-		</div>
-	</form>
-</div>
-
-<div id="pokedex">
-
-    <!--//// L E F T  P A R T ////-->
-    <div id="left">
-        <div id="logo"></div>
-        <div id="bg_curve1_left"></div>
-        <div id="bg_curve2_left"></div>
-        <div id="curve1_left">
-            <div id="buttonGlass">
-                <div id="reflect"> </div>
-            </div>
-            <div id="miniButtonGlass1"></div>
-            <div id="miniButtonGlass2"></div>
-            <div id="miniButtonGlass3"></div>
-        </div>
-        <div id="curve2_left">
-            <div id="junction">
-                <div id="junction1"></div>
-                <div id="junction2"></div>
-            </div>
-        </div>
-        <div id="screen">
-            <div id="topPicture">
-                <div id="buttontopPicture1"></div>
-                <div id="buttontopPicture2"></div>
-            </div>
-            <div id="picture">
-                <img id="sprite" src="<?php echo $pokesprite;?>"/>
-
-            </div>
-            <div id="buttonbottomPicture"></div>
-            <div id="speakers">
-                <div class="sp"></div>
-                <div class="sp"></div>
-                <div class="sp"></div>
-                <div class="sp"></div>
-            </div>
-        </div>
-        <div id="bigbluebutton"></div>
-        <div id="barbutton1"></div>
-        <div id="barbutton2"></div>
-        <div id="cross">
-            <div id="leftcross">
-                <div id="leftT"></div>
-            </div>
-            <div id="topcross">
-                <div id="upT"></div>
-            </div>
-            <div id="rightcross">
-                <div id="rightT"></div>
-            </div>
-            <div id="midcross">
-                <div id="midCircle"></div>
-            </div>
-            <div id="botcross">
-                <div id="downT"></div>
-            </div>
-        </div>
-    </div>
-
-    <!--//// R I G H T  P A R T ////-->
-    <div id="right">
-        <div id="stats" class="row">
-            <article>
-                <div class="col">
-                    <div id="submit-output">
-	                    <p><?php echo $pokeName;?></p>
-                        <p id="pokéId"><?php echo $pokeID;?></p>
-                    </div>
-
-                    <div id="moves">
-	                    <ul>
-		                    <li></li>
-		                    <li></li>
-		                    <li></li>
-		                    <li></li>
-	                    </ul>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="evolution" id="evolution">
-                    </div>
-                </div>
-            </article>
-        </div>
-        <div id="blueButtons1">
-
-        </div>
-
-        <div id="yellowBox1"></div>
-        <div id="yellowBox2"></div>
-        <div id="bg_curve1_right"></div>
-        <div id="bg_curve2_right"></div>
-        <div id="curve1_right"></div>
-        <div id="curve2_right"></div>
-    </div>
-</div>
-
-
-<script rel="script" src="script.js" lang="js"></script>
-</body>
-</html>
